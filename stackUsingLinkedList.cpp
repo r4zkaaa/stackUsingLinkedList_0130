c9bbf6ea89158b4a2fd847803559e7c2f7b0641c
r4zkaaa <rzkiiahmd11@gmail.com>
2026-05-14 11:54:59 +0700

    Menambahkan void pop

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -35,3 +35,12 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+      
