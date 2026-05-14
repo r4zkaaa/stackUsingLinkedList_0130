73969454b357c953fc65982e4e20e0cd5c6f31de
r4zkaaa <rzkiiahmd11@gmail.com>
2026-05-14 11:51:33 +0700

    Menambahkan data

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -26,4 +26,9 @@ public:
         top = NULL;
     }
 
-    
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+      
