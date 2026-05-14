0b12e0c60e55be8780df4703a32a139ea74e4f59
r4zkaaa <rzkiiahmd11@gmail.com>
2026-05-14 11:53:29 +0700

    Memperbarui pointer tap

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -31,4 +31,7 @@ public:
         Node *newNode = new Node();
         newNode->data = value;
         newNode->next = top;
-      
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
